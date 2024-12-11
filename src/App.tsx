7a26a803c1dedbe42d7df6d9af79777008fd5f8b
fdaniel-alvarez-dev <user01.eaglerw@gmail.com>
2024-12-11 04:51:52 -0500

    Solve some PortfolioGenerator warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import PortfolioGenerator from './components/PortfolioGenerator';
+import { PortfolioGenerator } from './components/PortfolioGenerator';
+import './App.css';
 
 function App() {
   return (
