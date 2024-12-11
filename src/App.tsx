bc0c45b25c5dc94860b176d75b2ea18a6da122b3
fdaniel-alvarez-dev <user01.eaglerw@gmail.com>
2024-12-11 04:25:04 -0500

    Solve some PortfolioGenerator warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import PortfolioGenerator from './components/PortfolioGenerator';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <PortfolioGenerator />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <PortfolioGenerator />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { PortfolioGenerator } from './components/PortfolioGenerator'; // nota la extensión .tsx
+import { PortfolioGenerator } from './components/PortfolioGenerator';  // Asegúrate que coincida con la exportación
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <PortfolioGenerator />
